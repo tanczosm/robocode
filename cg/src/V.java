69ba41cb361e072026f1019bd39e77bba8a543e9
tanczosm <mtanczos@gamedev.net>
2015-09-28 21:03:18 -0400

    Modified radar scanning to lock on to target

--- cg/src/V.java
+++ cg/src/V.java
@@ -1,7 +1,9 @@
 package cg;
 
 import robocode.AdvancedRobot;
+import robocode.Rules;
 import robocode.ScannedRobotEvent;
+import robocode.util.Utils;
 
 import java.awt.Color;
 
@@ -9,9 +11,15 @@ import static robocode.util.Utils.normalRelativeAngleDegrees;
 
 public class V extends AdvancedRobot {
 
+    private boolean turnRadar = true;
+
     public void run() {
 
-        setAllColors(Color.black);
+        setAdjustGunForRobotTurn(true);
+        setAdjustRadarForGunTurn(true);
+
+        //setAllColors(Color.black);
+        turnRadarRightRadians(Double.POSITIVE_INFINITY);
 
         while (true) {
             turnGunRight(Double.POSITIVE_INFINITY);
@@ -19,6 +27,17 @@ public class V extends AdvancedRobot {
     }
 
     public void onScannedRobot(ScannedRobotEvent e) {
+
+        double absBearing = e.getBearingRadians() + this.getHeadingRadians();
+
         setTurnGunRight(normalRelativeAngleDegrees(getHeading() + e.getBearing() - getGunHeading()));
+
+        if (turnRadar) {
+            double radarTurn = Utils.normalRelativeAngle(absBearing - this.getRadarHeadingRadians());
+            double extraTurn = Math.min(Math.atan(44.0 / e.getDistance()), Rules.RADAR_TURN_RATE_RADIANS);
+            radarTurn += (radarTurn < 0 ? -extraTurn : extraTurn);
+
+            this.setTurnRadarRightRadians(radarTurn);
+        }
     }
 }
