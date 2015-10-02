82ace2385ed0ce071222751c6eb365a48cfd63c6
greenec <connor.greene2000@gmail.com>
2015-10-01 20:15:54 -0400

    Added method to test network after training
    basicNetwork.compute() needs an MLData object as a parameter

--- cg/src/FreeformCompare.java
+++ cg/src/FreeformCompare.java
@@ -71,5 +71,6 @@ public class FreeformCompare {
                     + ", Basic: " + Format.formatPercent(basicTrain.getError()));
             sample++;
         }
+        // basicNetwork.compute(trainingSet);
     }
 }
